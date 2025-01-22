7572f27dce1699e029ddf80236a0b614617a8983
lighfe <juliandralle@gmx.net>
2025-01-22 14:46:18 +0100

    finalised step function using rk4

--- data_generation/models/tech_substitution.py
+++ data_generation/models/tech_substitution.py
@@ -3,7 +3,7 @@ from scipy.optimize import least_squares
 
 class TechnologySubstitution:
     def __init__(self, D0=1.0, delta=1.0, sigma=0.2, alpha=0.5, gamma1=1.0):
-        # 
+        # dimensions
         self.x_dim = 2 # x1, x2
         self.control_dim = 1 # gamma2
         # Params
@@ -20,13 +20,13 @@ class NumericalSolver:
     def __init__(self, model):
         self.model = model
 
-    def equilibrium_equations(self, vars, X, gamma2):
+    def equilibrium_equations(self, vars, X, control):
         """
         System of equations for multiple samples
         Params:
         vars: array of shape (n_samples * 3,) containing [y1, y2, p] for each sample
         X: array of shape (n_samples, 2) containing states [x1, x2]
-        gamma2: control parameter
+        control: array of shape (n_samples,) containing control values gamma2
         Returns:
         Array of constraint equations, shape (n_samples * 3,)
         """
@@ -40,29 +40,29 @@ class NumericalSolver:
         x1, x2 = X[:, 0], X[:, 1]
         x1 = np.maximum(x1, 1e-10)
         x2 = np.maximum(x2, 1e-10)
-
-        # TODO: Should gamma be a list of size n_samples, too?
         
         # Compute equations for all samples
         eq1 = self.model.gamma1 * y1**self.model.sigma / x1**self.model.alpha - p
-        eq2 = gamma2 * y2**self.model.sigma / x2**self.model.alpha - p
+        eq2 = control * y2**self.model.sigma / x2**self.model.alpha - p
         eq3 = y1 + y2 - (self.model.D0 - self.model.delta * p)
         
         # Stack equations back into 1D array
         return np.column_stack([eq1, eq2, eq3]).flatten()
     
-    def solve_equilibrium(self, X, gamma2):
+    def solve_equilibrium(self, X, control):
         """
         Numerical solve for production rates given observation and control
         
         Parameters:
         X: array of shape (n_samples, 2) containing observation [x1, x2]
-        gamma2: control parameter
+        control: control parameter gamma2
         
         Returns:
         Y: array of shape (n_samples, 2) containing production rates [y1, y2]
         """
         n_samples = X.shape[0]
+        assert control.shape == (n_samples,), \
+            f"Control shape {control.shape} doesn't match n_samples={n_samples}"
         
         # Initial guess for each sample
         p_guess = self.model.D0 / (2 * self.model.delta)
@@ -76,7 +76,7 @@ class NumericalSolver:
         result = least_squares(
             self.equilibrium_equations,
             initial_guess,
-            args=(X, gamma2),
+            args=(X, control),
             bounds=(lb, ub),
             method='trf',
             ftol=1e-6,
@@ -92,90 +92,43 @@ class NumericalSolver:
      
     def step(self, X, control, delta_t, num_steps=1):
         """
-        Generic step function working with any solver
+        Integrate system forward using rk4
 
         Parameters:
-        X: array of shape (n_samples, n_dim) or (n_dim,) containing observations [x1, x2]
-        gamma2: control parameter # TODO vector instead of single value
+        X: array of shape (n_samples, n_dim) containing observations [x1, x2]
+        control: array of shape (num_steps, n_samples) 
         delta_t: duration of timestep
-        solver: Solver class connected to a model
+        num_steps: number of timesteps to integrate
 
         Return:
-        Trajectory TODO
+        Trajectory: array of shape (num_steps+1, n_samples, n_dim) containing all observations
 
         """
-        X = np.atleast_2d(X)
-        Y = self.solve_equilibrium(X, control)
+        # NOTE: The Simulator needs to take care that all inputs come in the correct format
+        # possible TODO: step function can assume correct input, but maybe should add some asserts anyway
+
+
+        # Create empty trajectory
+        trajectory = np.zeros((num_steps + 1,) + X.shape)
+        # initialize starting observation
+        trajectory[0] = X
 
         # https://de.wikipedia.org/wiki/Klassisches_Runge-Kutta-Verfahren
-        # using simple version first
-        # TODO try more sophisticated scipy.integrate.solve_ivp
-
-        trajectory = np.zeros((num_steps + 1,) + X.shape)
-        trajectory[0] = X
+        # using simple version first because it's probably most performant
+        # NOTE: Might want to try more sophisticated scipy.integrate.solve_ivp
+        # related possible TODO: check if the calculations are accurate enough
         
         for step in range(num_steps):
             x = trajectory[step]
+            c = control[step] # shape (n, samples)
             
             # RK4 stages
-            k1 = self.solve_equilibrium(x, control)
-            k2 = self.solve_equilibrium(x + 0.5 * delta_t * k1, control)
-            k3 = self.solve_equilibrium(x + 0.5 * delta_t * k2, control)
-            k4 = self.solve_equilibrium(x + delta_t * k3, control)
+            k1 = self.solve_equilibrium(x, c)
+            k2 = self.solve_equilibrium(x + 0.5 * delta_t * k1, c)
+            k3 = self.solve_equilibrium(x + 0.5 * delta_t * k2, c)
+            k4 = self.solve_equilibrium(x + delta_t * k3, c)
             
             # Update
             trajectory[step + 1] = x + (delta_t / 6.0) * (k1 + 2*k2 + 2*k3 + k4)
 
         return trajectory
-
-def manual_test():
-    """Simple check if different inputs work"""
-    # TODO: Write a more sophisticated test, that also checks if the values are correct
-    model = TechnologySubstitution(D0=1.0, delta=1.0, sigma=0.2, alpha=0.5, gamma1=1.0)
-    solver = NumericalSolver(model)
-
-    test_cases = [
-        (np.array([[0.05, 0.01],[0.2, 0.05]]), 0.5),  # baseline case
-        (np.array([[2.0, 2.0],[2.0, 1.0]]), 1.0), # different gamma 
-        (np.array([2.0, 1.0]), 0.5), # (2,) dimensional
-        (np.array([[2.0, 1.0],[10.0, 0.5], [20.0, 18.0]]), 0.5) # (3,2) dimensional  
-    ]
-
-    for test in test_cases:
-        X, gamma2 = test
-        X_next = solver.step(X, control=gamma2, delta_t=1.0)
-        print(X_next)
-
-def test_integration():
-    """
-    Test the integration implementation
-    """
-    # Initialize model and solver with realistic parameters
-    model = TechnologySubstitution(D0=1.0, delta=1.0, sigma=0.2, alpha=0.5, gamma1=1.0)
-    solver = NumericalSolver(model)
-
-    # Test 1: Basic shape tests with realistic initial conditions
-    X_single = np.array([[0.5, 0.3]])  # Single sample, well above zero
-    X_multi = np.array([[0.5, 0.3], [0.7, 0.4], [0.2, 0.6]])  # Multiple samples
-    control = 0.5
-    dt = 0.1
-    
-    try:
-        # Single step trajectories
-        traj_single_step = solver.step(X_single, control, dt, num_steps=1)
-        print("Single step shape:", traj_single_step.shape)
-        print("Single step values:", traj_single_step)
-        
-        traj_multi_step = solver.step(X_multi, control, dt, num_steps=1)
-        print("Multi step shape:", traj_multi_step.shape)
-        
-        # Multiple timesteps
-        num_steps = 5
-        traj_multiple = solver.step(X_single, control, dt, num_steps=num_steps)
-        print("Multiple timesteps final value:", traj_multiple[-1])
-        
-        print("All tests passed successfully!")
-        
-    except Exception as e:
-        print(f"Test failed with error: {str(e)}")
-        raise
